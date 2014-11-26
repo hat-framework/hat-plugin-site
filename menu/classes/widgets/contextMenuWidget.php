14f5c0d05cbcf9c14d86d05c60a4fca0b5d2d6b2
tigredonorte <tigredonorte3@gmail.com>
2014-11-26 11:50:13 -0200

    context menu widget changed for mobile compatibility

--- menu/classes/widgets/contextMenuWidget.php
+++ menu/classes/widgets/contextMenuWidget.php
@@ -8,6 +8,7 @@ class contextMenuWidget extends \classes\Component\widget{
     
     protected function draw($itens){
         if(empty($itens)) {return '';}
+        $cid   = 'contextmenu_container';
         $class = 'nav nav-pills submenu';
         $li    = 'sublink dropdown';
         $lione = 'levelone';
@@ -15,9 +16,31 @@ class contextMenuWidget extends \classes\Component\widget{
         $data = classes\Classes\Template::getClass('contextmenu');
         if($this->id !== ""){$id = $this->id;}
         if($data !== "" && is_array($data)){extract($data);}
+        echo "<div id='container_btn_ctxmenu'>"
+                . "<button type='button' class='navbar-toggle collapsed btn btn-default col-xs-12' data-toggle='collapse' data-target='#$cid'>"
+                    . "<i class='fa fa-gear fa-2x'></i> Opções <i class='caret'></i>"
+                . "</button>"
+            . "</div>";
+        echo "<div id='$cid' class='collapse navbar-collapse'>";
         $this->LoadJsPlugin('menu/menu', 'mn')
                 ->setLiClass($li)
                 ->setLiOneClass($lione)
                 ->draw($itens, $class, $id);
+        echo "</div>";
     }
+    
+//    protected function draw($itens){
+//        if(empty($itens)) {return '';}
+//        $class = 'nav nav-pills submenu';
+//        $li    = 'sublink dropdown';
+//        $lione = 'levelone';
+//        $id    = $this->id;
+//        $data = classes\Classes\Template::getClass('contextmenu');
+//        if($this->id !== ""){$id = $this->id;}
+//        if($data !== "" && is_array($data)){extract($data);}
+//        $this->LoadJsPlugin('menu/menu', 'mn')
+//                ->setLiClass($li)
+//                ->setLiOneClass($lione)
+//                ->draw($itens, $class, $id);
+//    }
 }
