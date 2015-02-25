f9c1425e2a0a43ea85151bf6566d909287ebdc3f
tigredonorte <tigredonorte3@gmail.com>
2015-02-25 13:18:00 -0300

    sortFileManager

--- index/classes/indexFileManagerComponent.php
+++ index/classes/indexFileManagerComponent.php
@@ -4,13 +4,14 @@ class indexFileManagerComponent extends classes\Component\Component{
     
     public function showMenu($array, $base_url){
         $this->LoadJsPlugin('menu/treeview', 'tv');
+        ksort($array);
         echo "<div class='treeview'>";
         $this->displayMenu($array, $base_url);
         echo "</div>";
     }
     
     public function displayMenu($array, $base_url){
-        if(empty($array)) return "";
+        if(empty($array)) {return "";}
         echo "<ul>";
         foreach($array as $folder => $file){
             if(!is_array($file)) {
@@ -19,6 +20,7 @@ class indexFileManagerComponent extends classes\Component\Component{
                 continue;
             }
             echo "<li>$folder<ul>";
+            ksort($file);
             $this->displayMenu($file, "$base_url/$folder");
             echo "</ul></li>";
         }
